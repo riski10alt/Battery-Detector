ac1d7400c3a7dd88d69eb783fc8d98b3d8e65bef
Erisi <124305740+riski10alt@users.noreply.github.com>
2023-08-03 14:10:46 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ initBattery();
 function initBattery() {
     const batteryLiquid = document.querySelector(".Bliquid");
     const batteryStatus = document.querySelector(".Bstatus");
-    const Bperecentage = document.querySelector(".Bpercentage");
+    const Bpercentage = document.querySelector(".Bpercentage");
     navigator.getBattery().then((batt) => {
         updateBattery = () => {
             let level = Math.floor(batt.level * 100);
-            Bperecentage.innerHTML = level + "%";
-            batteryLiquid.style.height = `${parseInt(batt.level * 100)}$`
+            Bpercentage.innerHTML = level + "%";
+            batteryLiquid.style.height = `${parseInt(batt.level * 100)}%`;
             if (level == 100) {
-                batteryStatus.innerHTML = `Battery Full <i class="ri-battery-2-fill green color"></i>`;
+                batteryStatus.innerHTML = `Battery Full <i class="ri-battery-2-fill green-color"></i>`;
                 batteryLiquid.style.height = "103%";
             } else if (level <= 20 & !batt.charging) {
-                batteryStatus.innerHTML = `Low charge <i class="ri-plug-line animated-red animated-red"></i>`;
+                batteryStatus.innerHTML = `Low Charge <i class="ri-plug-line animated-red animated-red"></i>`;
             } else if (batt.charging) {
                 batteryStatus.innerHTML = `Charging ... <i class="ri-flashlight-line animated-green"></i>`;
             } else {
@@ -36,6 +36,6 @@ function initBattery() {
         }
         updateBattery();
         batt.addEventListener("chargingchange", () => { updateBattery() });
-        batt.addEventListener("levelchange", () => { updateBattery});    
+        batt.addEventListener("levelchange", () => { updateBattery });
     })
 }
